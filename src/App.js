15dcdce2a1e39c6fda57095f2fe979ab928f5d39
DMotta182 <motta2194@gmail.com>
2020-02-14 08:05:15 -0600

    React Router added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+//import { Route, Switch } from 'react-router-dom';
+
 function App() {
   return (
     <div>
