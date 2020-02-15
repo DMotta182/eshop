b5d080f6fa5650f3b7dd61084560c20801b067fd
DMotta182 <motta2194@gmail.com>
2020-02-14 19:48:06 -0600

    keeping going

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-//import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
